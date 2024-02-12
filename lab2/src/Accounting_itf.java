6d05bd49369356fe09b2eb2d5481ab49a6b6f4f7
keserz <110199920+keserz@users.noreply.github.com>
2024-02-12 13:26:12 +0100

    exo6

---
+++ lab2/src/Accounting_itf.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Accounting_itf extends Remote {
+    void numberOfCalls(int number) throws RemoteException;
+    int getCallCount() throws RemoteException; // Add this method declaration
+}
