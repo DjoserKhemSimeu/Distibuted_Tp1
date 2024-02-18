561b158e7fe7d5c890cb9ad82b7fbe081ec46377
keserz <110199920+keserz@users.noreply.github.com>
2024-02-18 20:43:39 +0100

    Delete lab2 (copie) directory

--- lab2 (copie)/src/Accounting_itf.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Accounting_itf extends Remote {
-    void numberOfCalls(int number) throws RemoteException;
-    int getCallCount() throws RemoteException; // Add this method declaration
-    void setCallCount(int i) throws RemoteException;
-}
